b418376178f629d33ad807ec2cfe80c0ad5fcea0
AbigailSc <abigailsarzuri@gmail.com>
2022-08-03 18:26:44 -0300

    Creando carpeta de themes para almacenar la paleta de colores para chakra

---
+++ src/resources/theme/index.js
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    primary: '#98D035',
+    secundary: '#E3FFB2',
+    text: '#18191F',
+    background: '#D9D9D9',
+    iconsColor: '#B8B8D2',
+    paginationBtn: '#F2F2F2',
+    backgroundBox: '#FFEBF0',
+    textSecundary: '#E6C1E3'
+  }
+}
+
+export const theme = extendTheme({ colors })
