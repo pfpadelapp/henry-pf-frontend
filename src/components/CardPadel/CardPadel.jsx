0e47057f41ba40aa92c1d4c023557c263d41f775
AbigailSc <abigailsarzuri@gmail.com>
2022-08-03 12:25:22 -0300

    Agrego componente card funcional

--- src/components/CardPadel/CardPadel.jsx
+++ src/components/CardPadel/CardPadel.jsx
@@ -1,7 +1,18 @@
-export default function CardPadel({ id }){
+import { Link } from "react-router-dom"
+
+export default function CardPadel({ id, location, image, owner }){
     return(
         <div>
-            <p>{id}</p>
+            <Link to={`detail/${id}`}>
+                <img src={image} alt={id}/>
+                <div>
+                    <h1>{owner}</h1>
+                    <div>
+                        <img/>
+                        <h3>{location}</h3>
+                    </div>
+                </div>
+            </Link>
         </div>
     )
 }
