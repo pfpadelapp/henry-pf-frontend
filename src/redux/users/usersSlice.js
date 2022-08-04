17672f2f9b11b0b9681040ab9aaff68e72e489dd
AbigailSc <abigailsarzuri@gmail.com>
2022-08-04 07:28:04 -0300

    componente de redux

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -2,15 +2,15 @@ import { createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 export const userSlice = createSlice({
-  name: 'users',
-  initialState: {
-    user: []
-  },
-  reducers: {
-    setUser: (state, action) => {
-      state.user = action.payload
+    name: 'users',
+    initialState: {
+        user: []
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload
+        }
     }
-  }
 })
 
 export const { setUser } = userSlice.actions
@@ -18,12 +18,12 @@ export const { setUser } = userSlice.actions
 export default userSlice.reducer
 
 export function fetchAllUsers() {
-  return async function(dispatch) {
-    try {
-      const allUsers = await axios.get('https://api-rest-server-padel.herokuapp.com/users')
-      dispatch(setUser(allUsers.data))
-    } catch (error) {
-      console.log(error)
+    return async function(dispatch) {
+        try {
+            const allUsers = await axios.get('https://api-rest-server-padel.herokuapp.com/users')
+            dispatch(setUser(allUsers.data))
+        } catch (error) {
+            console.log(error)
+        }
     }
-  }
 }
