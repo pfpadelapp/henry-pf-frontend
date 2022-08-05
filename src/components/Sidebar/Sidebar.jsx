2513b8fa3e31fc034138983020e855fb39ff75ed
unknown <matiasferrari96@gmail.com>
2022-08-05 20:13:25 -0300

    acomodando filtros

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,29 @@
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { FiMenu, FiHome, FiBell, FiFilter, FiClipboard, FiInfo } from "react-icons/fi";
-import { Flex , Avatar, Heading, Text, Divider, IconButton, Center, SimpleGrid} from '@chakra-ui/react'
+import { Flex , Menu, Link, MenuButton, useDisclosure, Button, Icon, Avatar, ModalCloseButton, ModalFooter, ModalBody, Heading, Text, Modal, IconButton, ModalOverlay, ModalContent, Stack, Select, ModalHeader} from '@chakra-ui/react'
 import NavItem from "../NavItem/NavItem"
+import {filterByType, orderByPrice, orderByAvailability } from '../../redux/padelField/padelFieldSlice'
 
 export default function Sidebar() {
-
+    const dispatch = useDispatch()
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const [navSize, changeNavSize] = useState("large")
+
+    function handleFilterType(e){
+        e.preventDefault();
+        dispatch(filterByType(e.target.value))
+    }
+    
+      function handleOrderPrice(e){
+        e.preventDefault();
+        dispatch(orderByPrice(e.target.value))
+    }
+      
+      function handleOrderAvailability(e){
+        e.preventDefault();
+        dispatch(orderByAvailability(e.target.value))
+    }
 
     return(
         <Flex 
@@ -36,11 +54,66 @@ export default function Sidebar() {
                 />
                     <NavItem navSize={navSize} icon={FiHome} title="Inicio" active/>
                     <NavItem navSize={navSize} icon={FiBell} title="Notificaciones"/>
-                    <NavItem navSize={navSize} icon={FiFilter} title="Filtrar"/>
+                    <Flex
+                        onClick={onOpen} 
+                        mt={30}
+                        flexDir="column"
+                        w="100%"
+                        aling-items={navSize == "small" ? "center" : "flex-start"}
+                    >
+                        <Menu placement='right'>
+                            <Link 
+                                backgroundColor={/*active &&*/ "none"}
+                                p={3}
+                                borderRadius={8}
+                                _hover={{textDecor:"none", background:"#ffff"}}
+                                w={navSize == "large" && "100%"}
+                            >
+                                <MenuButton w="100%">
+                                    <Flex>
+                                        <Icon as={FiFilter} fontSize="xl" color={/*active ? "#98D035" :*/ "gray.500"}/>
+                                        <Text ml={5} color={/*active ? "#98D035" : */"gray.500"} display={navSize == "small" ? "none" : "flex"}>Filtrar</Text>
+                                    </Flex>
+                                </MenuButton>
+                            </Link>
+                        </Menu>
+            
+                    </Flex>
                     <NavItem navSize={navSize} icon={FiClipboard} title="Turnos"/>
                     <NavItem navSize={navSize} icon={FiInfo} title="Contacto"/>
                     <NavItem navSize={navSize} icon={FiHome} title="Acerca de nosotros"/>
                 </Flex>
+                    
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <ModalContent>
+                        <ModalHeader>Filtro</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Stack spacing={3}>
+                                <Select variant='filled' placeholder='Tipo' size='md' onChange={e => handleFilterType(e)}>
+                                <option value='covered'>Techada</option>
+                                <option value='uncovered'>Sin Techo</option>
+                                </Select>
+                                <Select variant='filled' placeholder='Price' size='md' onChange={e => handleOrderPrice(e)}>
+                                <option value='1'>Ascendente</option>
+                                <option value='-1'>Descendiente</option>
+                                </Select>
+                                <Select variant='filled' placeholder='Disponibilidad' size='md' onChange={e => handleOrderAvailability(e)}>
+                                <option value='true'>Disponible</option>
+                                <option value='false'>No disponible</option>
+                                </Select>
+                            </Stack>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                 Cerrar
+                            </Button>
+                        </ModalFooter>
+                        </ModalContent>
+                    </Modal>
+                    
                 <Flex
                     p="5%"
                     flexDir="column"
