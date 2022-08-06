1e9888235cf3fdcea7d5bc3323c20769d354ba17
AbigailSc <abigailsarzuri@gmail.com>
2022-08-06 15:02:12 -0300

    Arreglo el responsive de las cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import { fetchAllUsers } from '../../redux/users/usersSlice'
 import { fetchAllPadelFields, filterByType, orderByPrice, orderByAvailability } from '../../redux/padelField/padelFieldSlice'
 import CardPadel from '../CardPadel/CardPadel.jsx'
 import Sidebar from "../Sidebar/Sidebar"
-import { Flex, Stack, Select, Avatar, Heading, Text, Divider, IconButton, Center, SimpleGrid, Spacer} from '@chakra-ui/react'
+import { Flex, Box, Stack, Select, Avatar, Heading, Text, Divider, IconButton, Center, SimpleGrid, Spacer} from '@chakra-ui/react'
 import { NavBar } from '../NavBar/NavBar'
 
 export default function Home() {
@@ -23,22 +23,22 @@ export default function Home() {
     <NavBar/>
     <Flex>
       <Sidebar/>
-        <Flex margin="2.5vh" w='100%' justifyContent='center'>
-          <SimpleGrid spacing={10} columns={3}>
-            {
-              allPadelField.padelField?.map((card) => (
-                <CardPadel
-                  key={card.id}
-                  id={card.id}
-                  location={card.location}
-                  image={card.image}
-                  name={card.name}
-                  type={card.type}
-                />
-              ))
-            }
-          </SimpleGrid>
-        </Flex>
+      <Flex margin="2.5vh 0" w='100%' justifyContent='center'>
+        <SimpleGrid spacing={50} columns={{ base: 1, lg: 2, xl: 3 }}>
+          {
+            allPadelField.padelField?.map((card) => (
+              <CardPadel
+                key={card.id}
+                id={card.id}
+                location={card.location}
+                image={card.image}
+                name={card.name}
+                type={card.type}
+              />
+            ))
+          }
+        </SimpleGrid>
+      </Flex>
     </Flex>
     </>
   )
