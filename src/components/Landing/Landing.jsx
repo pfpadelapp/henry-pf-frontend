bf0f1e2a0612a89bcd503278e20ba10132907a58
mmatiferrari <matiasferrari96@gmail.com>
2022-08-07 18:26:49 -0300

    actualizacionDiseño

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,33 +1,35 @@
-import { Flex, HStack, Spacer, Button, Text, Image} from '@chakra-ui/react'
+import { Flex, HStack, Spacer, Button, Text, Image, Center} from '@chakra-ui/react'
 import iphone from '../Images/phone.png'
 import { Link } from 'react-router-dom'
 
 export function Landing() {
   return (
         <>
-        <Flex height="60px" paddingLeft="100px" paddingRight="100px" backgroundColor="white" borderBottomColor="grey" borderBottomStyle="solid" borderBottomWidth="1px">
+        <Flex height="60px" paddingLeft="100px" paddingRight="100px" backgroundColor="white" borderBottomColor="#F8F1F1" borderBottomStyle="solid" borderBottomWidth="1px">
             <HStack as="nav" spacing="5">
                 <Link to="/home">
-                    <Button fontSize="15px" backgroundColor="white">Home</Button>
+                    <Button fontSize="15px" backgroundColor="white">Inicio</Button>
                 </Link>
-                <Button fontSize="15px" backgroundColor="white">Turno</Button>
+                <Button fontSize="15px" backgroundColor="white" >Turno</Button>
                 <Button fontSize="15px" backgroundColor="white">Contacto</Button>
-                <Button fontSize="15px" backgroundColor="white">Acerca de</Button>
+                <Button fontSize="15px" backgroundColor="white">Acerca de Nosotros</Button>
             </HStack>
             <Spacer/>
             <HStack as="nav" spacing="5">
-                <Button fontSize="15px" width="97px" height="35px" backgroundColor="#E3FFB2;">Login</Button>
-                <Button fontSize="15px" width="118px" height="35px" backgroundColor="#98D035;">Sign Up</Button>
+                <Button fontSize="15px" width="97px" height="35px" textColor="#98D035" backgroundColor="#E3FFB2;">Ingresar</Button>
+                <Button fontSize="15px" width="118px" height="35px"  textColor="#ffff" backgroundColor="#98D035">Registrarse</Button>
             </HStack>
         </Flex>
-        <Flex paddingTop="60px" justifyContent="center">
-            <Text color= "#18191F" fontSize="45px" fontWeight="800" fontStyle="normal">Descarga la app</Text>
-        </Flex>
-        <Flex justifyContent="center" paddingTop="20px">
-            <Image src="https://dctfcu.com/wp-content/uploads/sites/63/2018/04/google-play-logo-1518163351.png" h="40px" />
-        </Flex>
-        <Flex justifyContent="center" paddingTop="20px">
-            <Image src={iphone} height="355px" width="780px"/>
+        <Flex flexDir="column" h="95vh">
+            <Center paddingTop="60px" justifyContent="center">
+                <Text color= "#18191F" fontSize="45px" fontWeight="800" fontStyle="normal">Descarga la app</Text>
+            </Center>
+            <Center marginTop="30px">
+                <Image src="https://dctfcu.com/wp-content/uploads/sites/63/2018/04/google-play-logo-1518163351.png" h="40px" />
+            </Center >
+            <Center  marginTop="200px" >
+                <Image src={iphone}/>
+            </Center >
         </Flex>
         </>
   )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,7 @@ import SearchBar from '../Searchbar/SearchBar'
 import { Flex, Box, Spacer} from '@chakra-ui/react'
 export function NavBar() {
     return (
-       <Flex padding="30px 15px" height="45px" align="center" justifyContent="space.between" bgGradient='linear(to-l, #051937, #004d7a, #008793, #00bf72, #a8eb12)'>
-       <Box fontSize="25px">
-            LOGO
-       </Box>
+       <Flex padding="30px 15px" height="45px" align="center" justifyContent="space.between" >
        <Spacer/>
         <Box>
             <SearchBar/>
