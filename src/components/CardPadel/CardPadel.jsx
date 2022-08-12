744ccc7cf65aee91943fab7cd0effea2a0711091
AbigailSc <abigailsarzuri@gmail.com>
2022-08-12 13:47:51 -0300

    a

--- src/components/CardPadel/CardPadel.jsx
+++ src/components/CardPadel/CardPadel.jsx
@@ -16,12 +16,12 @@ export default function CardPadel({ id, location, image, name, price }) {
         <Image zIndex='-10' transition='all 1s' _hover={{ filter: 'brightness(0.7)', transition: 'all .5s ease' }} borderRadius='xl' width='sm' height='300px' fallbackSrc='https://via.placeholder.com/150' src={image} alt={id}/>
         <HStack justifyContent='space-between'>
           <Box>
-            <Text fontWeight='bold' fontSize='2xl'>
+            <Text fontWeight='bold' fontSize='2xl' textTransform='capitalize'>
               {name}
             </Text>
             <HStack as='span' color='gray.500' fontSize='md' fontWeight='medium'>
               <FaMapMarkerAlt/>
-              <Text>
+              <Text textTransform='capitalize'>
                 {location}
               </Text>
             </HStack>
