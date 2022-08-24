042fd5b589f5fc9f52f48a2c400654cd0f2175ca
fetichex <dfcmera@gmail.com>
2022-08-24 18:57:33 -0500

    fix(button-login): hiden button after login

--- src/components/LoginButton/LoginButton.jsx
+++ src/components/LoginButton/LoginButton.jsx
@@ -1,38 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { Button } from '@chakra-ui/react'
-import { useDispatch } from 'react-redux'
-import { useEffect, useState } from 'react'
 
 const LoginButton = () => {
-  const dispatch = useDispatch()
-  const { loginWithPopup, isAuthenticated, user } = useAuth0()
-  const [infoUser, setInfoUser] = useState()
-  console.log('user', user)
-  console.log(isAuthenticated)
+  const { loginWithPopup } = useAuth0()
 
-  // useEffect(() => {
-  //   const userLogged = localStorage.getItem('Usuario logeado')
-  //   if (userLogged !== 'undefined') {
-  //     console.log('este es userlogged', userLogged)
-  //     const user = JSON.parse(userLogged)
-  //     setInfoUser(user)
-  //     dispatch(getInfoLoginGoogle(user))
-  //   } else {
-  //     console.log('no funca')
-  //   }
-  // }, [])
-
-  // function handleLogin(e) {
-  //   e.preventDefault()
-  //   const userInfo = user
-  //   console.log('en el local, dentro del handle', userInfo)
-  //   localStorage.setItem(
-  //     'Usuario logeado', JSON.stringify(userInfo)
-  //   )
-
-  //   // dispatch(getInfoLoginGoogle(userFinal))
-  // }
-  // console.log('El estado del setuser es : ', infoUser)
   const login = async () => {
     await loginWithPopup()
   }
