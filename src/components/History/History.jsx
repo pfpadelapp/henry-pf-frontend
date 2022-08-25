c0743776142d1a838a9a8786b3555edbe8d2a8e9
Matias Ferrari <51061931+mmatiferrari@users.noreply.github.com>
2022-08-25 05:33:31 -0300

    agrego el history

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fetchAllUsers} from '../../redux/users/usersSlice'
 import { fetchAllPadelFields } from '../../redux/padelField/padelFieldSlice'
 import Sidebar from '../Sidebar/Sidebar'
-import { Flex, Spinner, Text, SimpleGrid } from '@chakra-ui/react'
+import { Flex, Spinner, Text, Center, Image, Box } from '@chakra-ui/react'
 import { NavBar } from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
 import ScrollToTop from 'react-scroll-to-top'
@@ -12,24 +12,40 @@ import { useNavigate } from 'react-router-dom'
 
 export default function History() {
   const dispatch = useDispatch()
-  const allUsers = useSelector((state) => state.users.users)
   const { isAuthenticated, isLoading, user } = useAuth0()
   const dataRender = useSelector((state) => state.users.userDetail)
   console.log("este es el datarender",dataRender)
 
 
 
-  useEffect(() => {
-    dispatch(fetchAllUsers())
-  }, [])
-
-
-
   return isLoading === true ? null : isAuthenticated ? (
-      <>
+    <>
       <Sidebar/>
       <NavBar/>
-
+      <Flex   width='100%'
+              justifyContent='center'
+              alignSelf='flex-start'>
+      { 
+      (dataRender?.history?.map((e=>{
+                        return(
+                                <Flex 
+                                    column={1}
+                                    justifyItems='center'
+                                    margin='4rem'
+                                    marginTop="15vh"
+                                    >
+                                      <Box>
+                                        <Image borderRadius="xl" height="300px" width="300px" objectFit="cover" src={e.newBooking.image}/>
+                                        <Text>Fecha: {e.newBooking.date.slice(0,10)}</Text>
+                                        <Text>Hora: {e.newBooking.date.slice(11,13)}hs</Text>
+                                        <Text color='brand.primary' fontWeight='bolder' fontSize='lg'>
+                                          ${e.newBooking.price}
+                                        </Text>
+                                      </Box>
+                                </Flex>
+                        )
+                    })))}
+        </Flex>
       <Footer/>
       <ScrollToTop/>
 
